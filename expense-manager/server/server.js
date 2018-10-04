ebef81453ccb9defad181eaa1f964e861040a069
Sushobhan04 <sushobhanghosh2021@u.northwestern.edu>
2018-10-03 23:41:17 -0500

    step 6 adding MongoDB support works

--- expense-manager/server/server.js
+++ expense-manager/server/server.js
@@ -1,9 +1,15 @@
+
 var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+mongoose.connect('mongodb://Susho04:Eecs497@ds121603.mlab.com:21603/green_project1');
 app.use('/', router);
 module.exports=app;
