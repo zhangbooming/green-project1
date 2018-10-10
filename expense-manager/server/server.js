3f08850b237e58d8a4be59cd4819eaa27ac03ca2
zhangbooming <BoyangZhang2020@u.northwestern.edu>
2018-10-10 14:35:46 -0500

    sum button and display

--- expense-manager/server/server.js
+++ expense-manager/server/server.js
@@ -10,7 +10,8 @@ app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
-mongoose.connect('mongodb://Susho04:Eecs497@ds121603.mlab.com:21603/green_project1');
-//mongoose.connect('mongodb://localhost:27017/green_project1');
+//mongoose.connect('mongodb://Susho04:Eecs497@ds121603.mlab.com:21603/green_project1');
+mongoose.connect('mongodb://localhost:27017/green_project1');
+//
 app.use('/', router);
 module.exports=app;
